afd75d2ff8dd37ab173b3614dd109f500b0368d7
Harshit K Mahaur <hkmnnit369@gmail.com>
2026-06-17 23:45:03 +0530

    Added Training script

--- Train.py
+++ Train.py
@@ -1,49 +1,62 @@
+import os
+import gymnasium as gym
+from gymnasium.envs.registration import register
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import DummyVecEnv
-import gymnasium as gym
-from gymnasium.envs.registration import register
-import os
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize 
 
 def main():
+
     log_dir = "./logs/"
     os.makedirs(log_dir, exist_ok=True)
 
     print("Initializing Environment...")
 
-    register(
-        id = 'Robot-Nav2-Env', # env name
-        entry_point = 'Robot_Env:RobotEscapeEnv'
-    )
+    # Prevent re-registration errors if running multiple times
+    try:
+        register(
+            id='Robot-RL-Env',
+            entry_point='Robot_Env:RobotEscapeEnv'
+        )
+    except Exception:
+        pass
 
-    raw_env = gym.make('Robot-Nav2-Env')
+    raw_env = gym.make('Robot-RL-Env')
 
     check_env(raw_env)
-    print("Environment check passed! No errors found.")
+    print("No errors found")
 
     env = Monitor(raw_env, log_dir)
     env = DummyVecEnv([lambda: env])
+    
+    # Normalize observations and rewards
+    env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.0)
 
-    print("Building the PPO Brain...")
+    print("Training the PPO algorithm...")
 
     model = PPO(
         "MlpPolicy", 
-        env, 
+        env,
         verbose=1, 
+        ent_coef=0.01,
+        clip_range=0.1,
         learning_rate=0.0003,
-        n_steps=4096,
-        batch_size=64,
+        n_steps=8192,
+        batch_size=256,
         tensorboard_log="./ppo_robot_tensorboard/" 
     )
 
     print("Starting Training Loop")
-    # 6. Train the model
-    model.learn(total_timesteps=4000000, tb_log_name="PPO_Run_1")
+    model.learn(total_timesteps=2000000, tb_log_name="PPO_Run_1")
 
     model_path = "ppo_local_planner_v1"
     model.save(model_path)
-    print(f"Training complete! Model saved successfully as {model_path}.zip")
+    print(f"Model saved successfully as {model_path}.zip")
+
+    stats_path = os.path.join(log_dir, "vec_normalize.pkl")
+    env.save(stats_path)
+    print(f"Normalization stats saved successfully as {stats_path}")
 
 if __name__ == "__main__":
     main()
